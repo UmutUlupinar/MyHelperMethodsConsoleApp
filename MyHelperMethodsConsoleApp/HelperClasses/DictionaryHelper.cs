416594f871d63e030db40138c13f4202faf73952
Umut Ulupinar <umutulupinarrr@gmail.com>
2023-11-07 23:45:28 +0300

    Update DictionaryHelper.cs
    dcitionary converter method-2

--- MyHelperMethodsConsoleApp/HelperClasses/DictionaryHelper.cs
+++ MyHelperMethodsConsoleApp/HelperClasses/DictionaryHelper.cs
@@ -55,6 +55,59 @@ public class PosData
 
         return posData;
     }
+
+    public static PosData ConvertDictionaryToObject(Dictionary<string, object> dictionary)
+{
+    PosData posData = new PosData();
+
+    // Dictionary içindeki değerleri PosData sınıfının özelliklerine atayın
+    if (dictionary.ContainsKey("pos_id") && dictionary["pos_id"] is string posId)
+    {
+        posData.pos_id = posId;
+    }
+
+    if (dictionary.ContainsKey("campaign_id") && dictionary["campaign_id"] is string campaignId)
+    {
+        posData.campaign_id = campaignId;
+    }
+
+    if (dictionary.ContainsKey("allocation_id") && dictionary["allocation_id"] is string allocationId)
+    {
+        posData.allocation_id = allocationId;
+    }
+
+    if (dictionary.ContainsKey("installments_number") && dictionary["installments_number"] is int installmentsNumber)
+    {
+        posData.installments_number = installmentsNumber;
+    }
+
+    if (dictionary.ContainsKey("amount_to_be_paid") && dictionary["amount_to_be_paid"] is decimal amountToBePaid)
+    {
+        posData.amount_to_be_paid = amountToBePaid;
+    }
+
+    if (dictionary.ContainsKey("currency_code") && dictionary["currency_code"] is string currencyCode)
+    {
+        posData.currency_code = currencyCode;
+    }
+
+    if (dictionary.ContainsKey("currency_id") && dictionary["currency_id"] is string currencyId)
+    {
+        posData.currency_id = currencyId;
+    }
+
+    if (dictionary.ContainsKey("title") && dictionary["title"] is string title)
+    {
+        posData.title = title;
+    }
+
+    if (dictionary.ContainsKey("hash_key") && dictionary["hash_key"] is string hashKey)
+    {
+        posData.hash_key = hashKey;
+    }
+
+    return posData;
+}
     
     
 }
